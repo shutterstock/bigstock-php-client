58746ee81ad5b5d2753d9ac272d501b8565f344e
Brent Baisley <brenttech@gmail.com>
2014-12-12 11:06:24 -0500

    Formatting cleanup

--- src/client.php
+++ src/client.php
@@ -11,7 +11,8 @@ namespace Bigstock;
 use Shutterstock\Presto\Presto;
 use Shutterstock\Presto\Response;
 
-class Bigstock {
+class Bigstock
+{
     public $rest = null;
     protected $url_base = 'api.bigstockphoto.com/2/';
     public $url = null;
@@ -24,29 +25,31 @@ class Bigstock {
      * @param $secret_key
      * @param null $curl_opts
      */
-    public function __construct($account_id, $secret_key, $curl_opts=null) {
-       $opts = array(
-            CURLOPT_NOSIGNAL=>1,
-            CURLOPT_TIMEOUT_MS=>2000
+    public function __construct($account_id, $secret_key, $curl_opts = null)
+    {
+        $opts = array(
+            CURLOPT_NOSIGNAL => 1,
+            CURLOPT_TIMEOUT_MS => 2000
         );
-        if ( is_array($curl_opts) ) {
+        if (is_array($curl_opts)) {
             $opts = array_merge($opts, $curl_opts);
         }
         $this->rest = new Presto($opts);
         $this->account_id = $account_id;
         $this->secret_key = $secret_key;
-        $this->url_base .= $account_id.'/';
+        $this->url_base .= $account_id . '/';
         $this->url = $this->url_base;
     }
 
     /**
      * @param $mode anything other than 'prod' will use 'test' mode
      */
-    public function setMode($mode) {
-        if ($mode=='prod') {
+    public function setMode($mode)
+    {
+        if ($mode == 'prod') {
             $this->url = $this->url_base;
         } else {
-            $this->url = 'test'.$this->url_base;
+            $this->url = 'test' . $this->url_base;
         }
     }
 
@@ -54,7 +57,8 @@ class Bigstock {
      * @param $params
      * @return mixed|object
      */
-    public function search($params) {
+    public function search($params)
+    {
         $url = 'http://' . $this->url_base . '/search/?';
         $url .= http_build_query($params);
         $r = $this->rest->get($url);
@@ -66,7 +70,8 @@ class Bigstock {
      * @param string $type
      * @return mixed|object
      */
-    public function getAsset($id, $type='image') {
+    public function getAsset($id, $type = 'image')
+    {
         $url = $this->url . $type . '/' . $id;
         $r = $this->rest->get($url);
         return $this->processResponse($r);
@@ -76,7 +81,8 @@ class Bigstock {
      * @param $id
      * @return mixed|object
      */
-    public function getImage($id) {
+    public function getImage($id)
+    {
         return $this->getAsset($id, 'image');
     }
 
@@ -84,7 +90,8 @@ class Bigstock {
      * @param $id
      * @return mixed|object
      */
-    public function getVideo($id) {
+    public function getVideo($id)
+    {
         return $this->getAsset($id, 'video');
     }
 
@@ -92,7 +99,8 @@ class Bigstock {
      * @param string $type
      * @return mixed|object
      */
-    public function getCollections($type='image') {
+    public function getCollections($type = 'image')
+    {
         $url = $this->url . $type . '/?auth_key=' . $this->generateAuthKey();
         $r = $this->rest->get($url);
         return $this->processResponse($r);
@@ -104,13 +112,14 @@ class Bigstock {
      * @param null $params
      * @return mixed|object
      */
-    public function getCollection($type, $id, $params=null) {
-        if ( is_array($params) ) {
+    public function getCollection($type, $id, $params = null)
+    {
+        if (is_array($params)) {
             $params['auth_key'] = $this->generateAuthKey($id);
         } else {
-            $params = array('auth_key'=>$this->generateAuthKey($id));
+            $params = array('auth_key' => $this->generateAuthKey($id));
         }
-        $url = $this->url . $type . '/'. $id . '/?' . http_build_query($params);
+        $url = $this->url . $type . '/' . $id . '/?' . http_build_query($params);
         $r = $this->rest->get($url);
         return $this->processResponse($r);
     }
@@ -118,7 +127,8 @@ class Bigstock {
     /**
      * @return mixed|object
      */
-    public function getLightboxes() {
+    public function getLightboxes()
+    {
         return $this->getCollections('lightbox');
     }
 
@@ -127,14 +137,16 @@ class Bigstock {
      * @param null $params
      * @return mixed|object
      */
-    public function getLightbox($id, $params=null) {
+    public function getLightbox($id, $params = null)
+    {
         return $this->getCollection('lightbox', $id, $params);
     }
 
     /**
      * @return mixed|object
      */
-    public function getClipboxes() {
+    public function getClipboxes()
+    {
         return $this->getCollections('video');
     }
 
@@ -143,7 +155,8 @@ class Bigstock {
      * @param null $params
      * @return mixed|object
      */
-    public function getClipbox($id, $params=null) {
+    public function getClipbox($id, $params = null)
+    {
         return $this->getCollection('clipbox', $id, $params);
     }
 
@@ -151,7 +164,8 @@ class Bigstock {
      * @param string $lang
      * @return mixed|object
      */
-    public function getCategories($lang="en") {
+    public function getCategories($lang = "en")
+    {
         $url = $this->url . 'categories/?language=' . $lang;
         $r = $this->rest->get($url);
         return $this->processResponse($r);
@@ -160,16 +174,17 @@ class Bigstock {
     /**
      * @param $id   asset ID to purchase
      * @param $size_code    usually s, m, l, xl
-     * @param string $type  image or video
+     * @param string $type image or video
      * @return mixed|object
      */
-    public function getPurchase($id, $size_code, $type='image') {
+    public function getPurchase($id, $size_code, $type = 'image')
+    {
         $params = array(
-            $type.'_id'=>$id,
-            'size_code'=>$size_code,
-            'auth_key'=>$this->generateAuthKey($id)
+            $type . '_id' => $id,
+            'size_code' => $size_code,
+            'auth_key' => $this->generateAuthKey($id)
         );
-        $url = $this->url . '/purchase/?'.http_build_query($params);
+        $url = $this->url . '/purchase/?' . http_build_query($params);
         $r = $this->rest->get($url);
         return $this->processResponse($r);
     }
@@ -178,20 +193,22 @@ class Bigstock {
      * @param $download_id
      * @return string
      */
-    public function getDownloadUrl($download_id) {
+    public function getDownloadUrl($download_id)
+    {
         $params = array(
-            'auth_key'=>$this->generateAuthKey($download_id),
-            'download_id'=>$download_id
+            'auth_key' => $this->generateAuthKey($download_id),
+            'download_id' => $download_id
         );
         $url = $this->url . 'download?' . http_build_query($params);
         return $url;
-   }
+    }
 
     /**
      * @param $download_id
      * @return mixed|object
      */
-    public function download($download_id) {
+    public function download($download_id)
+    {
         $url = $this->getDownloadUrl($download_id);
         $r = $this->rest->get($url);
         return $this->processResponse($r);
@@ -201,8 +218,9 @@ class Bigstock {
      * @param null $id
      * @return string
      */
-    public function generateAuthKey($id=null) {
-        if ( is_null($id) ) {
+    public function generateAuthKey($id = null)
+    {
+        if (is_null($id)) {
             $auth_key = sha1($this->secret_key . $this->account_id);
         } else {
             $auth_key = sha1($this->secret_key . $this->account_id . $id);
@@ -214,19 +232,20 @@ class Bigstock {
      * @param $r
      * @return mixed|object
      */
-    public function processResponse($r) {
+    public function processResponse($r)
+    {
         $this->raw_response = $r;
-        if ( $r->meta['is_success'] ) {
-            if ( strpos($r->meta['content_type'], 'json')===false ) {
+        if ($r->meta['is_success']) {
+            if (strpos($r->meta['content_type'], 'json') === false) {
                 $r = $r->data;
             } else {
                 $r = json_decode($r->data);
             }
         } else {
-            $r = (object) array(
-                'response_code'=>$r->meta['errorno'],
-                'message'=>$r->meta['error'],
-                'data'=>array()
+            $r = (object)array(
+                'response_code' => $r->meta['errorno'],
+                'message' => $r->meta['error'],
+                'data' => array()
             );
         }
         return $r;
