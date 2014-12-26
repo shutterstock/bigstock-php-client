5a230594c076e09994d4b6cfd2811561bc6704a2
Patrick Schwisow <patrick.schwisow@gmail.com>
2014-12-26 14:55:59 -0600

    Add docblocks and clean-up.

--- src/client.php
+++ src/client.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: bbaisley
- * Date: 12/11/14
- * Time: 5:17 PM
+ * Bigstock API Client
+ *
+ * @package   Bigstock
+ * @author    Brent Baisley <bbaisley@shutterstock.com>
+ * @copyright 2014 Brent Baisley
+ * @license   http://opensource.org/licenses/MIT MIT License
  */
 
 namespace Bigstock;
@@ -13,62 +15,103 @@ use Shutterstock\Presto\Response;
 
 class Bigstock
 {
+    /**
+     * REST Client
+     *
+     * @var Presto
+     */
     public $rest = null;
+
+    /**
+     * The base URL for API requests
+     *
+     * @var string
+     */
     protected $url_base = 'api.bigstockphoto.com/2/';
+
+    /**
+     * Currently set URL for API requests (based on environment)
+     *
+     * @var string
+     */
     public $url = null;
-    public $raw_response = null;
+
+    /**
+     * Response from the last API call
+     *
+     * @var mixed
+     */
+    private $raw_response = null;
+
+    /**
+     * API Account ID
+     *
+     * @var string
+     */
     private $account_id = null;
+
+    /**
+     * API Secret Key
+     *
+     * @var string
+     */
     private $secret_key = null;
 
     /**
-     * @param $account_id
-     * @param $secret_key
-     * @param null $curl_opts
+     * Constructor
+     *
+     * @param string $account_id
+     * @param string $secret_key
+     * @param string $mode
+     * @param array  $curl_opts
      */
-    public function __construct($account_id, $secret_key, $curl_opts = null)
+    public function __construct($account_id, $secret_key, $mode = 'prod', $curl_opts = array())
     {
         $opts = array(
             CURLOPT_NOSIGNAL => 1,
             CURLOPT_TIMEOUT_MS => 2000
         );
-        if (is_array($curl_opts)) {
+        if (!empty($curl_opts)) {
             $opts = array_merge($opts, $curl_opts);
         }
         $this->rest = new Presto($opts);
         $this->account_id = $account_id;
         $this->secret_key = $secret_key;
         $this->url_base .= $account_id . '/';
-        $this->url = $this->url_base;
+        $this->setMode($mode);
     }
 
     /**
-     * @param $mode anything other than 'prod' will use 'test' mode
+     * Set client mode (test or prod)
+     *
+     * @param string $mode anything other than 'prod' will use 'test' mode
      */
     public function setMode($mode)
     {
-        if ($mode == 'prod') {
-            $this->url = $this->url_base;
-        } else {
-            $this->url = 'test' . $this->url_base;
-        }
+        $this->url = 'http://' . ($mode == 'prod' ? '' : 'test') . $this->url_base . $this->account_id . '/';
     }
 
     /**
-     * @param $params
-     * @return mixed|object
+     * API Call: search
+     *
+     * @see http://help.bigstockphoto.com/hc/en-us/articles/200303245-API-Documentation#search
+     *
+     * @param array $params
+     * @return mixed
      */
-    public function search($params)
+    public function search(array $params)
     {
-        $url = 'http://' . $this->url_base . '/search/?';
-        $url .= http_build_query($params);
+        $url = $this->url . 'search/?' . http_build_query($params);
         $r = $this->rest->get($url);
         return $this->processResponse($r);
     }
 
     /**
-     * @param $id
+     * API Call: get asset
+     *
+     * @param string $id
      * @param string $type
-     * @return mixed|object
+     * @return mixed
      */
     public function getAsset($id, $type = 'image')
     {
@@ -78,8 +121,12 @@ class Bigstock
     }
 
     /**
-     * @param $id
-     * @return mixed|object
+     * API Call: get image
+     *
+     * @see http://help.bigstockphoto.com/hc/en-us/articles/200303245-API-Documentation#image-detail
+     *
+     * @param string $id
+     * @return mixed
      */
     public function getImage($id)
     {
@@ -87,8 +134,10 @@ class Bigstock
     }
 
     /**
-     * @param $id
-     * @return mixed|object
+     * API Call: get video
+     *
+     * @param string $id
+     * @return mixed
      */
     public function getVideo($id)
     {
@@ -96,8 +145,10 @@ class Bigstock
     }
 
     /**
+     * API Call: get collections
+     *
      * @param string $type
-     * @return mixed|object
+     * @return mixed
      */
     public function getCollections($type = 'image')
     {
@@ -107,25 +158,27 @@ class Bigstock
     }
 
     /**
-     * @param $type
-     * @param $id
-     * @param null $params
-     * @return mixed|object
+     * API Call: get collections
+     *
+     * @param string $type
+     * @param string $id
+     * @param array  $params
+     * @return mixed
      */
-    public function getCollection($type, $id, $params = null)
+    public function getCollection($type, $id, array $params = array())
     {
-        if (is_array($params)) {
-            $params['auth_key'] = $this->generateAuthKey($id);
-        } else {
-            $params = array('auth_key' => $this->generateAuthKey($id));
-        }
+        $params['auth_key'] = $this->generateAuthKey($id);
         $url = $this->url . $type . '/' . $id . '/?' . http_build_query($params);
         $r = $this->rest->get($url);
         return $this->processResponse($r);
     }
 
     /**
-     * @return mixed|object
+     * API Call: get lightboxes
+     *
+     * @see http://help.bigstockphoto.com/hc/en-us/articles/200303245-API-Documentation#lightbox
+     *
+     * @return mixed
      */
     public function getLightboxes()
     {
@@ -133,17 +186,23 @@ class Bigstock
     }
 
     /**
-     * @param $id
-     * @param null $params
-     * @return mixed|object
+     * API Call: get lightbox
+     *
+     * @see http://help.bigstockphoto.com/hc/en-us/articles/200303245-API-Documentation#lightbox
+     *
+     * @param string $id
+     * @param array  $params
+     * @return mixed
      */
-    public function getLightbox($id, $params = null)
+    public function getLightbox($id, array $params = array())
     {
         return $this->getCollection('lightbox', $id, $params);
     }
 
     /**
-     * @return mixed|object
+     * API Call: get clipboxes
+     *
+     * @return mixed
      */
     public function getClipboxes()
     {
@@ -151,18 +210,24 @@ class Bigstock
     }
 
     /**
-     * @param $id
-     * @param null $params
-     * @return mixed|object
+     * API Call: get clipbox
+     *
+     * @param string $id
+     * @param array  $params
+     * @return mixed
      */
-    public function getClipbox($id, $params = null)
+    public function getClipbox($id, array $params = array())
     {
         return $this->getCollection('clipbox', $id, $params);
     }
 
     /**
+     * API Call: get categories
+     *
+     * @see http://help.bigstockphoto.com/hc/en-us/articles/200303245-API-Documentation#categories
+     *
      * @param string $lang
-     * @return mixed|object
+     * @return mixed
      */
     public function getCategories($lang = "en")
     {
@@ -172,10 +237,14 @@ class Bigstock
     }
 
     /**
-     * @param $id   asset ID to purchase
-     * @param $size_code    usually s, m, l, xl
-     * @param string $type image or video
-     * @return mixed|object
+     * API Call: get purchase
+     *
+     * @see http://help.bigstockphoto.com/hc/en-us/articles/200303245-API-Documentation#purchase
+     *
+     * @param string $id        asset ID to purchase
+     * @param string $size_code usually s, m, l, xl
+     * @param string $type      image or video
+     * @return mixed
      */
     public function getPurchase($id, $size_code, $type = 'image')
     {
@@ -190,7 +259,11 @@ class Bigstock
     }
 
     /**
-     * @param $download_id
+     * API Call: get download URL
+     *
+     * @see http://help.bigstockphoto.com/hc/en-us/articles/200303245-API-Documentation#download
+     *
+     * @param string $download_id
      * @return string
      */
     public function getDownloadUrl($download_id)
@@ -204,8 +277,12 @@ class Bigstock
     }
 
     /**
-     * @param $download_id
-     * @return mixed|object
+     * API Call: get download
+     *
+     * @see http://help.bigstockphoto.com/hc/en-us/articles/200303245-API-Documentation#download
+     *
+     * @param string $download_id
+     * @return mixed
      */
     public function download($download_id)
     {
@@ -215,7 +292,9 @@ class Bigstock
     }
 
     /**
-     * @param null $id
+     * Generate an API auth key
+     *
+     * @param null|string $id
      * @return string
      */
     public function generateAuthKey($id = null)
@@ -229,8 +308,10 @@ class Bigstock
     }
 
     /**
-     * @param $r
-     * @return mixed|object
+     * Process / decode the raw response
+     *
+     * @param Response $r
+     * @return mixed
      */
     public function processResponse($r)
     {
